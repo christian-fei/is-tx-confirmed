1121990f24786820e43bfc011b6d10385bbe5ccf
Christian Fei <crifei93@gmail.com>
2019-10-19 11:34:06 +0200

    add test for isConfirmed

--- index.test.js
+++ index.test.js
@@ -1,4 +1,40 @@
-const test = require('tap')
+const { test, todo } = require('tap')
+const https = require('https')
 
-test.todo('true if tx is confirmed')
-test.todo('false if tx is not confirmed')
+test('true if tx is confirmed', async t => {
+  const confirmed = await isConfirmed('57fafe15fcca248ea56331069f5002b4d1611e37a5ad0095a70e645935699b35')
+  t.ok(confirmed)
+  t.end()
+})
+
+test('false if tx is not confirmed', async t => {
+  const confirmed = await isConfirmed('3446102f825d4c5706760592a4709cee69b410eef4a95e64fc6bec5ac7f7b37b')
+  t.notOk(confirmed)
+  t.end()
+})
+
+async function isConfirmed (address) {
+  return new Promise((resolve, reject) => {
+    https.get(`https://live.blockcypher.com/ltc/tx/${address}/`, (res) => {
+      const buffer = []
+      res.on('data', d => buffer.push(d))
+      res.on('end', () => {
+        const string = Buffer.concat(buffer).toString('utf8')
+        resolve(string)
+      })
+    })
+      .on('error', (err) => {
+        console.error(err)
+        reject(err)
+      })
+  })
+    .then(body => {
+      if (body.includes('num-confs')) {
+        const match = body.match(/num-confs">(\d+)/)
+        if (!match) return false
+        const confirmations = match[1]
+        return confirmations >= 6
+      }
+      return false
+    })
+}
